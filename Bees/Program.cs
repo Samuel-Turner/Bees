0414cbe0a12b1d1e4af9e8b9090cd8fd4153fb37
Sam turner <samturnerturnersam@outlook.com>
2021-02-03 17:15:43 +0000

    Part1

--- Bees/Program.cs
+++ Bees/Program.cs
@@ -6,11 +6,6 @@ namespace Bees
     {
         static void Main(string[] args)
         {
-            Bee bee1 = new Bee();
-            bee1.Damage(5);
-            bee1.Damage(5);
-            //Assert.AreEqual(95, bee1.Health);
-            Console.WriteLine(bee1.Health);
             Console.ReadLine();
         }
     }
