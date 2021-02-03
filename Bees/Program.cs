f0844e9d0a2ff84958e15953dc4129747ded9fca
Sam turner <samturnerturnersam@outlook.com>
2021-02-03 16:11:18 +0000

    Add project files.

---
+++ Bees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
